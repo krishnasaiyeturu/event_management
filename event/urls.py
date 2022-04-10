d3b2ee21b43a69d02b4cf7ff104377a716b7320a
Krishna sai <krishnasaiyeturu@gmail.com>
2022-04-10 11:54:20 +0530

    dfjkefjk

---
+++ event/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from . import views
+
+# from django.urls import path
+
+# Your path of views
+
+
+urlpatterns = [
+
+    url('login', views.login),
+    url(r'^logout/$', views.logout_view),
+
+    url(r'^dashboard', views.index),
+
+]
---
+++ event/views.py
@@ -0,0 +1,26 @@
+from django.db.models import query
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate,logout
+
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+
+
+@csrf_exempt
+def login(request):
+    logout(request)
+    return render(request,'login.html')
+
+@csrf_exempt
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
+# @login_required(login_url='/login')
+def index(request):
+    return render(request,'index.html')
+
