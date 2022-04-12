71f53f3f5da8e06195b64cd32dddfe2f8f11080c
Krishna sai <krishnasaiyeturu@gmail.com>
2022-04-12 17:05:27 +0530

    models chnaged

--- event/models.py
+++ event/models.py
@@ -6,6 +6,18 @@ from django.contrib.auth.models import User as MainUser
 
 
 # Create your models here.
+class EventManager(models.Model):
+    user = models.OneToOneField(MainUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    contact = models.CharField(unique=True,max_length=12)
+    email = models.EmailField()
+    designation = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+# Create your models here.
 class Customer(models.Model):
     user = models.OneToOneField(MainUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
@@ -38,7 +50,28 @@ class Venue(models.Model):
 
 class EstimatedPrice(models.Model):
     event_type = models.ForeignKey(EventType,on_delete=models.CASCADE)
+    venue = models.ForeignKey(Venue,on_delete=models.CASCADE)
     no_of_participants = models.PositiveIntegerField()
+    estimation_price = models.PositiveIntegerField()
+       
+    def __str__(self):
+        return self.event_type.name
+
+
+EVENT_STATUS = (
+    ('Accepted', 'Accepted'),
+    ('Requested', 'Requested'),
+    ('Rejected', 'Rejected')
+)
+
+class Event(models.Model):
+    date_of_event = models.DateField()
+    no_of_participants = models.PositiveIntegerField()
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    event_type = models.ForeignKey(EventType,on_delete=models.CASCADE)
+    venue = models.ForeignKey(Venue,on_delete=models.CASCADE)
+    status = models.CharField(max_length=50, choices=EVENT_STATUS, default='Requested')
+    duration = models.PositiveIntegerField()
     estimation_price = models.PositiveIntegerField()
        
     def __str__(self):
